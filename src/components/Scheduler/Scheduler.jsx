f583858da995ee807dab31a3d2f7547d10acc272
Marit Zelinsky <maritzelinsky@me.com>
2019-09-16 11:13:48 -0500

    sets local sttate to team when selected in drop down menu

--- src/components/Scheduler/Scheduler.jsx
+++ src/components/Scheduler/Scheduler.jsx
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Scheduler extends Component {
+    componentDidMount() {
+        this.props.dispatch({ type: 'FETCH_TEAMS' })
+    }
+
+    state = {
+        selectedTeam: {
+            name: ''
+        }
+    }
+
+    handleChange(event) {
+        console.log(event.target.value)
+        this.setState({name: event.target.value});
+    }
+
     render () {
         return (
             <div>
                 <h1>Scheduler</h1>
+                <select onChange={(event) => this.handleChange(event)}>
+                {this.props.state.teamsReducer && this.props.state.teamsReducer.map(team => {
+                    return (
+                        <option key={team.name}>{team.name}</option>
+                        )
+                    })}
+                </select>
             </div>
         )
     }
